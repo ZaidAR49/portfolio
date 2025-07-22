c150394a4d8bc7b58e5c2b33d758abe009480e18
ZaidAR49 <148114243+ZaidAR49@users.noreply.github.com>
2025-07-22 22:02:09 +0300

    Update projects.tsx

--- client/src/components/projects.tsx
+++ client/src/components/projects.tsx
@@ -4,20 +4,25 @@ import { useState } from "react";
 export const Projects = () => {
   const [index_image, setIndex_image] = useState(0);
 
-  const handleImageChange = (direction: "next" | "prev",projectindex:number) => {
+  const handleImageChange = (
+    direction: "next" | "prev",
+    projectindex: number
+  ) => {
     if (direction === "next") {
-      setIndex_image((prev) => (prev + 1) % projects[projectindex].image.length);
+      setIndex_image(
+        (prev) => (prev + 1) % projects[projectindex].image.length
+      );
     } else {
       setIndex_image((prev) =>
         prev === 0 ? projects[projectindex].image.length - 1 : prev - 1
       );
     }
   };
-  const stateColor = ( index: number) => {
-    switch (projects[index].state) {
+  const stateColor = (index: number) => {
+    switch (projects[index].state.toLowerCase()) {
       case "completed":
         return "bg-green-500 text-white";
-      case "in-progress":
+      case "in progress":
         return "bg-yellow-500 text-white";
       case "suspend":
         return "bg-red-500 text-white";
@@ -26,67 +31,114 @@ export const Projects = () => {
     }
   };
   return (
-     <>
-    <div  id="projects" className="flex flex-col p-4 sm:py-6 lg:py-10 my-16 sm:my-24 lg:my-32 ">
+    <>
+      <div
+        id="projects"
+        className="flex flex-col p-4 sm:py-6 lg:py-10 my-16 sm:my-24 lg:my-32 "
+      >
         <div className=" p-10 sm:px-32 max-w-3xl ">
-            <div className="header-primary text-3xl sm:text-4xl lg:text-6xl">Projects</div>
-            <br/>
-            <p className="text-base sm:text-lg lg:text-xl">Here are some of the selected projects that showcase my passion for front-end development</p>
+          <div className="header-primary text-3xl sm:text-4xl lg:text-6xl">
+            Projects
+          </div>
+          <br />
+          <p className="text-base sm:text-lg lg:text-xl">
+            Here are some of the selected projects that showcase my passion for
+            front-end development
+          </p>
         </div>
-{projects.map((project, index) => (
-  <div key={index} className="flex flex-col lg:flex-row justify-between p-10 sm:p-32">
-    <div className="w-full lg:w-[550px] h-[300px] sm:h-[400px] lg:h-[550px] bg-slate-900 flex justify-center rounded-xl center items-center relative">
-      <div className={`absolute top-2 left-2 p-2 rounded-xl text-xs sm:text-sm ${stateColor(index)}`}>{project.state}</div>
-    <div id="slide1" className="carousel-item relative flex items-center justify-center">
-  <img
-    src={project.image[index_image]}
-    className="h-48 w-48 sm:h-64 sm:w-64 lg:h-96 lg:w-96 rounded-xl object-fill"
-    alt="project"
-  />
+        {projects.map((project, index) => (
+          <div
+            key={index}
+            className="flex flex-col lg:flex-row justify-between p-10 lg:p-32 xl:p-14  md:p-16"
+          >
+            <div className="w-full lg:w-[550px] h-[300px] sm:h-[400px] lg:h-[550px] bg-slate-900 flex justify-center rounded-xl center items-center relative">
+              <div
+                className={`absolute top-2 left-2 p-2 rounded-xl text-xs sm:text-sm ${stateColor(
+                  index
+                )}`}
+              >
+                {project.state}
+              </div>
+              <div
+                id="slide1"
+                className="carousel-item relative flex items-center justify-center"
+              >
+                <img
+                  src={project.image[index_image]}
+                  className="h-48 w-48 sm:h-64 sm:w-64 lg:h-96 lg:w-96 rounded-xl object-fill"
+                  alt="project"
+                />
 
-  <div className="absolute inset-0 flex items-center justify-between -mx-8 sm:-mx-12 lg:-mx-16">
-    <div onClick={()=>{handleImageChange("prev",index); console.log(index)}} className="text-2xl sm:text-3xl text-white hover:scale-125 transition cursor-pointer select-none">❮</div>
-    <div onClick={()=>{handleImageChange("next",index); console.log(index)}} className="text-2xl sm:text-3xl text-white hover:scale-125 transition cursor-pointer select-none">❯</div>
-  </div>
-</div>
+                <div
+                  className="absolute inset-0 flex items-center justify-between -mx-8 sm:-mx-12 lg:-mx-16"
+                  onKeyDown={(e) => {
+                    if (e.key === "ArrowRight") {
+                      handleImageChange("next", index);
+                    } else if (e.key === "ArrowLeft") {
+                      handleImageChange("prev", index);
+                    }
+                  }}
+                  tabIndex={0}
+                   role="button"
+                >
+                  <div
+                    onClick={() => {
+                      handleImageChange("prev", index);
+                    }}
+                    className="text-2xl sm:text-3xl text-white hover:scale-125 transition cursor-pointer select-none"
+                  >
+                    ❮
+                  </div>
+                  <div
+                    onClick={() => {
+                      handleImageChange("next", index);
+                    }}
+                    className="text-2xl sm:text-3xl text-white hover:scale-125 transition cursor-pointer select-none"
+                  >
+                    ❯
+                  </div>
+                </div>
+              </div>
+            </div>
+            {/*project desc*/}
+            <div className="flex flex-col gap-4 sm:gap-6 lg:gap-7 mt-2 lg:mt-5 flex-nowrap max-w-2xl">
+              <div className="header-secondary text-2xl sm:text-3xl lg:text-4xl">
+                {project.title}{" "}
+              </div>
 
+              <p className="text-base sm:text-lg lg:text-xl">
+                {project.description}
+              </p>
+              <div className="bg-black text-white p-4 sm:p-6 max-w-xl border border-gray-800">
+                <h2 className="text-sm sm:text-base font-semibold mb-4">
+                  PROJECT INFO
+                </h2>
+
+                <div className="border-t border-gray-600 py-3 flex justify-between text-sm sm:text-base">
+                  <span>Year</span>
+                  <span className="text-gray-300">{project.year}</span>
+                </div>
+
+                <div className="border-t border-gray-600 py-3 flex justify-between text-sm sm:text-base">
+                  <span>Role</span>
+                  <span className="text-gray-300">{project.role}</span>
+                </div>
+                <div className="border-t border-gray-600 py-3 flex justify-between text-sm sm:text-base">
+                  <span>Techs</span>
+                  <span className="text-gray-300">{project.tech}</span>
+                </div>
+              </div>
+              <a
+                className="btn-primary w-fit my-3 sm:my-4 lg:my-5 text-sm sm:text-base"
+                href="https://github.com/ZaidAR49/Green-Bridge.git"
+              >
+                <FaGithub /> view in Github
+              </a>
+            </div>
+          </div>
+        ))}
       </div>
-    {/*project desc*/}
-    <div className="flex flex-col gap-4 sm:gap-6 lg:gap-7 mt-2 lg:mt-5 flex-nowrap max-w-2xl">
-<div className="header-secondary text-2xl sm:text-3xl lg:text-4xl">{project.title} </div>
-
-<p className="text-base sm:text-lg lg:text-xl">{project.description}</p>
- <div className="bg-black text-white p-4 sm:p-6 max-w-xl border border-gray-800">
-      <h2 className="text-sm sm:text-base font-semibold mb-4">PROJECT INFO</h2>
-
-    
-
-      <div className="border-t border-gray-600 py-3 flex justify-between text-sm sm:text-base">
-        <span >Year</span>
-        <span className="text-gray-300">{project.year}</span>
-      </div>
-
-      <div className="border-t border-gray-600 py-3 flex justify-between text-sm sm:text-base">
-        <span >Role</span>
-        <span className="text-gray-300">{project.role}</span>
-      </div>
-        <div className="border-t border-gray-600 py-3 flex justify-between text-sm sm:text-base">
-        <span >Techs</span>
-        <span className="text-gray-300">{project.tech}</span>
-      </div>
-
-    </div>
-<a className="btn-primary w-fit my-3 sm:my-4 lg:my-5 text-sm sm:text-base" href="https://github.com/ZaidAR49/Green-Bridge.git"><FaGithub/> view in Github</a>
-    </div>
-    
-    
-    
-    
-</div>
-))}
-      
-    </div>
-    <div className="w-full h-px bg-white opacity-30"></div>
-   </>
+      <div className="w-full h-px bg-white opacity-30"></div>
+    </>
   );
-}
+};
