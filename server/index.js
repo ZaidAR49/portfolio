97799d237bf0acc4dbcbba1746460cbdf0672086
ZaidAR49 <148114243+ZaidAR49@users.noreply.github.com>
2025-07-25 20:23:36 +0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -21,10 +21,21 @@ const transporter = nodemailer.createTransport({
 app.post('/api/contact', (req, res) => {
     const { name, email, subject, message } = req.body;
     
-    const emailBodyTouser=`<div style="color:rgb(70, 70, 70)">
-  <h3>Dear ${name}</h3><p>Thank you for reaching out! We have received your message and will 
-    get back to you shortly.</p><p>Best regards,<br>Zaid Radaideh</p> 
-</div>`;
+    const emailBodyTouser=`<div style="max-width:600px; margin:auto; font-family:Arial, sans-serif; background-color:#000; color:#fff; padding:20px; border-radius:10px; box-shadow:0 0 10px rgba(0,0,0,0.5);">
+  <h2 style="color:#eee;">We've Received Your Messag</h2>
+
+  <hr style="border: 1px solid #333; margin:20px 0;" />
+
+  <div style="background:#111; padding:15px; border-radius:8px;">
+    <h3 style="color:#eee;">Dear ${name}</h3>
+    <p style="color:#eee;">Thank you for reaching out! We have received your message and will get back to you shortly.</p>
+
+    <p style="color:#eee;">Best regards,<br />Zaid Radaideh</p>
+       <a href="https://zar.onrender.com" style="display:inline-block; background:rgb(6,182,212); border-radius:5px; color:#fff; padding:12px 20px; text-decoration:none; font-weight:bold;">Go Back to Portfolio</a>
+
+  </div>
+</div>
+`;
 
     const emailBodyToOwner=`<div style="max-width:600px; margin:auto; font-family:Arial, sans-serif; background-color:#000; color:#fff; padding:20px; border-radius:10px; box-shadow:0 0 10px rgba(0,0,0,0.5);">
   <h2 style=" color:#eee"> message from your portfolio</h2>
