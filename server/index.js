baf4a92f7ad84c0ee8a894550948d0018e3a34ff
ZaidAR49 <148114243+ZaidAR49@users.noreply.github.com>
2025-07-26 01:15:09 +0300

    project done

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import nodemailer from 'nodemailer';
 import dotenv from 'dotenv';
+import { emailBodyToOwner, emailBodyTouser } from './component/email-template.js';
 dotenv.config();
 const app = express();
 const port = 3000;
@@ -20,57 +21,23 @@ const transporter = nodemailer.createTransport({
 
 app.post('/api/contact', (req, res) => {
     const { name, email, subject, message } = req.body;
+    const portfolioUrl = 'https://zar.onrender.com';
+   
+
     
-    const emailBodyTouser=`<div style="max-width:600px; margin:auto; font-family:Arial, sans-serif; background-color:#000; color:#fff; padding:20px; border-radius:10px; box-shadow:0 0 10px rgba(0,0,0,0.5);">
-  <h2 style="color:#eee;">We've Received Your Messag</h2>
-
-  <hr style="border: 1px solid #333; margin:20px 0;" />
-
-  <div style="background:#111; padding:15px; border-radius:8px;">
-    <h3 style="color:#eee;">Dear ${name}</h3>
-    <p style="color:#eee;">Thank you for reaching out! We have received your message and will get back to you shortly.</p>
-
-    <p style="color:#eee;">Best regards,<br />Zaid Radaideh</p>
-       <a href="https://zar.onrender.com" style="display:inline-block; background:rgb(6,182,212); border-radius:5px; color:#fff; padding:12px 20px; text-decoration:none; font-weight:bold;">Go Back to Portfolio</a>
-
-  </div>
-</div>
-`;
-
-    const emailBodyToOwner=`<div style="max-width:600px; margin:auto; font-family:Arial, sans-serif; background-color:#000; color:#fff; padding:20px; border-radius:10px; box-shadow:0 0 10px rgba(0,0,0,0.5);">
-  <h2 style=" color:#eee"> message from your portfolio</h2>
-  
-
-  <hr style="border: 1px solid #333; margin:20px 0;" />
-
-  <div style="background:#111; padding:15px; border-radius:8px;">
-    <h3 style="color:#eee;">📌 Subject</h3>
-    <p style="color:#eee;">${subject}</p>
-
-    <h3 style="color:#eee;">👤 Name</h3>
-    <p style="color:#eee;">${name}</p>
-
-    <h3 style="color:#eee;">📧 Email</h3>
-    <p style="color:#eee;">${email}</p>
-
-    <h3 style="color:#eee;">💬 Message</h3>
-    <p style="color:#eee;">${message}</p>
-  </div>
-
-</div>`
 
     const emailToOwner = {
         from: process.env.EMAIL, // replace with your email
          replyTo: email, 
         to: process.env.EMAIL, 
         subject: `Contact Form portfolio `,
-        html: emailBodyToOwner
+        html: emailBodyToOwner(subject, name, email, message)
     };
     const emailToUser = {
         from: process.env.EMAIL, 
         to: email, 
         subject: 'Thank you for contacting us!',
-        html: emailBodyTouser
+        html: emailBodyTouser(name,portfolioUrl ) // replace with your portfolio URL
     };
 
     
