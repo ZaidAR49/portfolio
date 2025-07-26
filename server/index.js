dfa0a611b544f8d1fe8bcef855330e29ac545793
ZaidAR49 <148114243+ZaidAR49@users.noreply.github.com>
2025-07-26 12:11:40 +0300

    server deploy

--- server/index.js
+++ server/index.js
@@ -27,7 +27,7 @@ app.post('/api/contact', (req, res) => {
     
 
     const emailToOwner = {
-        from: process.env.EMAIL, // replace with your email
+        from: process.env.EMAIL, 
          replyTo: email, 
         to: process.env.EMAIL, 
         subject: `Contact Form portfolio `,
